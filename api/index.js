727c64d5f0f26cbb0cdc06e5b27cdd1b0ae7420c
Gargi Singh <gargi_singh@icloud.com>
2023-02-06 12:36:13 +0530

    user reg. connected to cloud db

--- api/index.js
+++ api/index.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+const User = require('./models/User');
 const app = express();
 
-app.get('/resgister', (req,res) => {
-    res.json('test okay');
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://mern-blog:CwgWHaIihPDul8q1@cluster0.dxpdq5b.mongodb.net/?retryWrites=true&w=majority');
+
+app.post('/register', async (req,res) => {
+    const {username, password} = req.body;
+    try{
+     const userDoc = await User.create({username, password});
+     res.json(userDoc);
+    }catch(e){
+        res.status(400).json(e);
+    }
+
 });
 
+
 app.listen(4000);
+
+// CwgWHaIihPDul8q1
+// mongodb+srv://mern-blog:CwgWHaIihPDul8q1@cluster0.dxpdq5b.mongodb.net/?retryWrites=true&w=majority
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,8 +1,20 @@
+import {useState} from "react";
+
 export default function LoginPage(){
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    function register(){
-
+    async function register(ev){
+        ev.preventDefault();
+        const resposne = await fetch('http://localhost:4000/register',{
+            method: 'POST',
+            body: JSON.stringify({username,password}),
+            headers: {'Content-Type': 'application/json'},
+        })
+        if (resposne.status ===200) {
+            alert('registration successful');
+        } else {
+            alert('registration failed');
+        }
     }
 
     return(
