2d1e3eb8927a36b37e2adaec02ab785646498002
Gargi Singh <gargi_singh@icloud.com>
2023-03-07 19:28:07 +0530

    working again!

--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -2,20 +2,12 @@ import { useEffect, useState } from "react";
 import Post from "../post";
 
 export default function IndexPage(){
-    const [post, setPosts] = useState([]);
-    useEffect(() => {
-        fetch('http://localhost:4000/post').then(response => {
-            response.json().then(posts => {
-                console.log(posts);
-            });
-        });
-    }, []);
-
+    
     return(
         <>
-        {posts.length > 0 && posts.map(post => (
-            <Post{...post}/>
-        ))}
+        <Post/>
+        <Post/>
+        <Post/>
         </>
     );
 }
